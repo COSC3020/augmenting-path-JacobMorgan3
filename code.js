944ffb01a096a6419470013c0844ea98462f246d
JacobMorgan3 <144738756+JacobMorgan3@users.noreply.github.com>
2024-04-04 16:23:19 -0600

    Working on function, not finished yet (i jsut wanted to save my work)

--- code.js
+++ code.js
@@ -1,3 +1,25 @@
 function augmentingPath(graph, start, end) {
+    if (start == end) { //if start and end are the same, then retrun that node in a list
+        return [start];
+    }
+    let path = [];
+    
+    for (let [node, connections] of Object.entries(graph)) {
+        for (let [node, connections] of Object.entries(graph)) {
+            path.push(node);
+            if (node == end)
+                return path;
+        }
+    } 
+    console.log(path);
     return [];
 }
+
+
+let graph = {'foo': {'boo': 7, 'd': 4}, //graph from code.test.js
+             'boo': {'e': 3},
+             'd': {'bar': 4},
+             'e': {'foo': 1},
+             'bar': {'foo': 2}};
+             
+console.log(augmentingPath(graph, 'foo', 'bar'));
